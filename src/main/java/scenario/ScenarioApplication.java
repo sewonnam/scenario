d0f9e6e6453357264c878930e0918ca30286eaea
sewonnam <nswon.github@gmail.com>
2024-06-12 23:22:35 +0900

    initial commit

---
+++ src/main/java/scenario/ScenarioApplication.java
@@ -0,0 +1,13 @@
+package scenario;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScenarioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScenarioApplication.class, args);
+	}
+
+}
---
+++ src/test/java/scenario/ScenarioApplicationTests.java
@@ -0,0 +1,13 @@
+package scenario;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScenarioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
